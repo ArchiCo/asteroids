132186c138fb4473ac8802793a3e09121fc475b5
VicRoman97 <38137876+VicRoman97@users.noreply.github.com>
2018-05-23 16:45:40 +0200

    no i dont have to have a commit message

--- src/Asteroids.java
+++ src/Asteroids.java
@@ -49,6 +49,17 @@ import java.applet.AudioClip;
 
 public class Asteroids extends Applet implements Runnable, KeyListener {
 
+	//Constructor
+	missle missle1;
+
+	 
+	
+	
+	
+	
+	
+	
+	
   // Copyright information.
 
   String copyName = "Asteroids";
@@ -137,7 +148,6 @@ public class Asteroids extends Applet implements Runnable, KeyListener {
   boolean detail;
 
   // Key flags.
-
   boolean left  = false;
   boolean right = false;
   boolean up    = false;
@@ -148,11 +158,11 @@ public class Asteroids extends Applet implements Runnable, KeyListener {
   AsteroidsSprite   ship;
   AsteroidsSprite   fwdThruster, revThruster;
   AsteroidsSprite   ufo;
-  AsteroidsSprite   missle;
+  //AsteroidsSprite   missle;
   AsteroidsSprite[] photons    = new AsteroidsSprite[MAX_SHOTS];
   AsteroidsSprite[] asteroids  = new AsteroidsSprite[MAX_ROCKS];
   AsteroidsSprite[] explosions = new AsteroidsSprite[MAX_SCRAP];
-
+  
   // Ship data.
 
   int shipsLeft;       // Number of ships left in game, including current one.
@@ -169,9 +179,6 @@ public class Asteroids extends Applet implements Runnable, KeyListener {
   int ufoPassesLeft;    // Counter for number of flying saucer passes.
   int ufoCounter;       // Timer counter used to track each flying saucer pass.
 
-  // Missle data.
-
-  int missleCounter;    // Counter for life of missle.
 
   // Asteroid data.
 
@@ -720,103 +727,103 @@ public class Asteroids extends Applet implements Runnable, KeyListener {
       saucerSound.stop();
     saucerPlaying = false;
   }
-
-  public void initMissle() {
-
-    missle.active = true;
-    missle.angle = 0.0;
-    missle.deltaAngle = 0.0;
-    missle.x = ufo.x;
-    missle.y = ufo.y;
-    missle.deltaX = 0.0;
-    missle.deltaY = 0.0;
-    missle.render();
-    missleCounter = MISSLE_COUNT;
-    if (sound)
-      missleSound.loop();
-    misslePlaying = true;
-  }
-
-  public void updateMissle() {
-
-    int i;
-
-    // Move the guided missle and check for collision with ship or photon. Stop
-    // it when its counter has expired.
-
-    if (missle.active) {
-      if (--missleCounter <= 0)
-        stopMissle();
-      else {
-        guideMissle();
-        missle.advance();
-        missle.render();
-        for (i = 0; i < MAX_SHOTS; i++)
-          if (photons[i].active && missle.isColliding(photons[i])) {
-            if (sound)
-              crashSound.play();
-            explode(missle);
-            stopMissle();
-            score += MISSLE_POINTS;
-          }
-        if (missle.active && ship.active &&
-            hyperCounter <= 0 && ship.isColliding(missle)) {
-          if (sound)
-            crashSound.play();
-          explode(ship);
-          stopShip();
-          stopUfo();
-          stopMissle();
-        }
-      }
-    }
-  }
-
-  public void guideMissle() {
-
-    double dx, dy, angle;
-
-    if (!ship.active || hyperCounter > 0)
-      return;
-
-    // Find the angle needed to hit the ship.
-
-    dx = ship.x - missle.x;
-    dy = ship.y - missle.y;
-    if (dx == 0 && dy == 0)
-      angle = 0;
-    if (dx == 0) {
-      if (dy < 0)
-        angle = -Math.PI / 2;
-      else
-        angle = Math.PI / 2;
-    }
-    else {
-      angle = Math.atan(Math.abs(dy / dx));
-      if (dy > 0)
-        angle = -angle;
-      if (dx < 0)
-        angle = Math.PI - angle;
-    }
-
-    // Adjust angle for screen coordinates.
-
-    missle.angle = angle - Math.PI / 2;
-
-    // Change the missle's angle so that it points toward the ship.
-
-    missle.deltaX = 0.75 * MAX_ROCK_SPEED * -Math.sin(missle.angle);
-    missle.deltaY = 0.75 * MAX_ROCK_SPEED *  Math.cos(missle.angle);
-  }
-
-  public void stopMissle() {
-
-    missle.active = false;
-    missleCounter = 0;
-    if (loaded)
-      missleSound.stop();
-    misslePlaying = false;
-  }
+//
+//  public void initMissle() {
+//
+//    missle.active = true;
+//    missle.angle = 0.0;
+//    missle.deltaAngle = 0.0;
+//    missle.x = ufo.x;
+//    missle.y = ufo.y;
+//    missle.deltaX = 0.0;
+//    missle.deltaY = 0.0;
+//    missle.render();
+//    missleCounter = MISSLE_COUNT;
+//    if (sound)
+//      missleSound.loop();
+//    misslePlaying = true;
+//  }
+//
+//  public void updateMissle() {
+//
+//    int i;
+//
+//    // Move the guided missle and check for collision with ship or photon. Stop
+//    // it when its counter has expired.
+//
+//    if (missle.active) {
+//      if (--missleCounter <= 0)
+//        stopMissle();
+//      else {
+//        guideMissle();
+//        missle.advance();
+//        missle.render();
+//        for (i = 0; i < MAX_SHOTS; i++)
+//          if (photons[i].active && missle.isColliding(photons[i])) {
+//            if (sound)
+//              crashSound.play();
+//            explode(missle);
+//            stopMissle();
+//            score += MISSLE_POINTS;
+//          }
+//        if (missle.active && ship.active &&
+//            hyperCounter <= 0 && ship.isColliding(missle)) {
+//          if (sound)
+//            crashSound.play();
+//          explode(ship);
+//          stopShip();
+//          stopUfo();
+//          stopMissle();
+//        }
+//      }
+//    }
+//  }
+//
+//  public void guideMissle() {
+//
+//    double dx, dy, angle;
+//
+//    if (!ship.active || hyperCounter > 0)
+//      return;
+//
+//    // Find the angle needed to hit the ship.
+//
+//    dx = ship.x - missle.x;
+//    dy = ship.y - missle.y;
+//    if (dx == 0 && dy == 0)
+//      angle = 0;
+//    if (dx == 0) {
+//      if (dy < 0)
+//        angle = -Math.PI / 2;
+//      else
+//        angle = Math.PI / 2;
+//    }
+//    else {
+//      angle = Math.atan(Math.abs(dy / dx));
+//      if (dy > 0)
+//        angle = -angle;
+//      if (dx < 0)
+//        angle = Math.PI - angle;
+//    }
+//
+//    // Adjust angle for screen coordinates.
+//
+//    missle.angle = angle - Math.PI / 2;
+//
+//    // Change the missle's angle so that it points toward the ship.
+//
+//    missle.deltaX = 0.75 * MAX_ROCK_SPEED * -Math.sin(missle.angle);
+//    missle.deltaY = 0.75 * MAX_ROCK_SPEED *  Math.cos(missle.angle);
+//  }
+//
+//  public void stopMissle() {
+//
+//    missle.active = false;
+//    missleCounter = 0;
+//    if (loaded)
+//      missleSound.stop();
+//    misslePlaying = false;
+//  }
 
   public void initAsteroids() {
 
